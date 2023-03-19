1768b1e73c65e361c78b00935456f0d3bfe903dc
sirekanian <sirekanian@pm.me>
2023-03-19 22:13:22 +0400

    init

---
+++ app/src/main/java/org/sirekanyan/q1506050/MainActivity.java
@@ -0,0 +1,15 @@
+package org.sirekanyan.q1506050;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
